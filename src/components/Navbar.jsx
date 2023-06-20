db95a5d388f88971097ef294fa96fe0aa8acff68
Reol <reolraju@gmail.com>
2023-06-21 00:48:25 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Container = styled.div`
+heigth:100px;
+background-color:black;
+color:white;
+`
+
+const Navbar = () => {
+  return (
+    <Container>
+      NAvbar
+    </Container>
+     
+    
+  )
+}
+
+export default Navbar
